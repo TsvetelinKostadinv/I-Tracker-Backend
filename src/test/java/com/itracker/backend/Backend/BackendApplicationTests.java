00232edcb77fc8e3bcd55b2075b6b447382c05e5
Tsvetelin Kostadinov <tsvetelinkostadinovts@gmail.com>
2020-03-25 00:32:16 +0200

    Initial commit with buildship stuff

---
+++ src/test/java/com/itracker/backend/Backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itracker.backend.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
