8ba2bf69d277cc02f0f2b64ee377c1e1711696f7
Tsvetelin Kostadinov <tsvetelinkostadinovts@gmail.com>
2020-03-25 22:10:36 +0200

    Fixed the invoice controller so it has all the basic CRUD functionalities Removed extra rows in .gitignore

--- src/main/java/com/itracker/backend/Backend/controllers/InvoiceController.java
+++ src/main/java/com/itracker/backend/Backend/controllers/InvoiceController.java
@@ -5,23 +5,23 @@
 package com.itracker.backend.Backend.controllers;
 
 
-import java.sql.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.itracker.backend.Backend.entities.invoice.InvoiceEntity;
 import com.itracker.backend.Backend.entities.invoice.view.InvoiceView;
-import com.itracker.backend.Backend.entities.payment.Payment;
 import com.itracker.backend.Backend.repositories.InvoiceRepository;
-import com.itracker.backend.Backend.util.timeutil.Time;
 
 
 /**
@@ -32,31 +32,129 @@ import com.itracker.backend.Backend.util.timeutil.Time;
 public class InvoiceController
 {
     
-    @Autowired
-    InvoiceRepository repo;
+    private final InvoiceRepository repo;
     
-    @GetMapping ( "/test" )
-    public InvoiceEntity index ()
+    @Autowired
+    public InvoiceController ( InvoiceRepository repo )
     {
-        final InvoiceEntity res =
-            new InvoiceEntity( 3l , "202929386" , new Date( 0 ) , "Benzinostaciq" , "CIG Trans" , "disel" , 5000.0 );
-        
-        res.addPayment( new Payment( Time.nowSQL() , 2500.0 ) );
-        res.addPayment( new Payment( Time.nowSQL() , 2500.0 ) );
-        
-        return res;
+        this.repo = repo;
     }
     
     @PostMapping ( "/create" )
-    public ResponseEntity< InvoiceEntity > create ( @RequestBody InvoiceView view )
+    public ResponseEntity< InvoiceEntity > create (
+        @RequestBody InvoiceView view
+    )
     {
-        return new ResponseEntity< InvoiceEntity >( repo.save( view.toEntity() ) , HttpStatus.OK );
+        return new ResponseEntity< InvoiceEntity >(
+            repo.save( view.toEntity() ) ,
+            HttpStatus.OK
+        );
+    }
+    
+    @GetMapping ( "/get/{id}" )
+    public ResponseEntity< InvoiceEntity > getById (
+        @PathVariable ( value = "id" ) Long id
+    )
+    {
+        return repo
+            .findById( id )
+            .map(
+                x -> new ResponseEntity< InvoiceEntity >(
+                    x ,
+                    HttpStatus.OK
+                )
+            )
+            .orElse( new ResponseEntity< InvoiceEntity >( HttpStatus.NOT_FOUND ) );
+    }
+    
+    @DeleteMapping ( "/delete/{id}" )
+    public ResponseEntity< InvoiceEntity > deleteById (
+        @PathVariable ( value = "id" ) Long id
+    )
+    {
+        repo.deleteById( id );
+        return new ResponseEntity< InvoiceEntity >( HttpStatus.OK );
+    }
+    
+    @PostMapping ( "/update/{id}" )
+    public ResponseEntity< InvoiceEntity > update (
+        @PathVariable ( value = "id" ) Long id ,
+        @RequestBody InvoiceView view
+    )
+    {
+        repo.deleteById( id );
+        final InvoiceEntity ent = view.toEntity();
+        ent.setId( id );
+        return new ResponseEntity< InvoiceEntity >(
+            repo.save( ent ) ,
+            HttpStatus.OK
+        );
     }
     
     @GetMapping ( "/all" )
-    public List< InvoiceView > all (  )
+    public List< InvoiceEntity > all ()
     {
-        return repo.findAll().stream().map( InvoiceView::new ).collect( Collectors.toList() );
+        return repo.findAll();
+    }
+    
+    @GetMapping ( "/all/view" )
+    public List< InvoiceView > allViews ()
+    {
+        return repo
+            .findAll()
+            .stream()
+            .map( InvoiceView::new )
+            .collect( Collectors.toList() );
+    }
+    
+    @GetMapping ( "/first" )
+    public List< InvoiceEntity > first ( @PathVariable Long number )
+    {
+        return repo
+            .findAll()
+            .stream()
+            .limit( number )
+            .collect( Collectors.toList() );
+    }
+    
+    @GetMapping ( "/first/view" )
+    public List< InvoiceView > firstViews ( @PathVariable Long number )
+    {
+        return repo
+            .findAll()
+            .stream()
+            .limit( number )
+            .map( InvoiceView::new )
+            .collect( Collectors.toList() );
+    }
+    
+    @GetMapping ( "/first" )
+    public List< InvoiceEntity > first (
+        @NonNull @PathVariable Long after ,
+        @NonNull @PathVariable Long number
+    )
+    {
+        return repo
+            .findAll()
+            .stream()
+            .skip( after )
+            .limit( number )
+            .collect( Collectors.toList() );
+    }
+    
+    @GetMapping ( "/first/view" )
+    public List< InvoiceView > firstViews (
+        @NonNull @PathVariable Long after ,
+        @NonNull @PathVariable Long number
+    )
+    {
+        return repo
+            .findAll()
+            .stream()
+            .skip( after )
+            .limit( number )
+            .map( InvoiceView::new )
+            .collect( Collectors.toList() );
     }
     
 }
